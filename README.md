b4954ce177010eb5899b3cd2e47311990f79c8a7
Jerry Ling <proton@jling.dev>
2021-06-25 00:49:40 +0200

    init commit, able to get 200 return from a event.json endpoin

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Indicomb
 
-[![Stable](https://img.shields.io/badge/docs-stable-blue.svg)](https://Jerry Ling.github.io/Indicomb.jl/stable)
-[![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://Jerry Ling.github.io/Indicomb.jl/dev)
-[![Build Status](https://github.com/Jerry Ling/Indicomb.jl/workflows/CI/badge.svg)](https://github.com/Jerry Ling/Indicomb.jl/actions)
+[![Stable](https://img.shields.io/badge/docs-stable-blue.svg)](https://Moelf.github.io/Indicomb.jl/stable)
+[![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://Moelf.github.io/Indicomb.jl/dev)
+[![Build Status](https://github.com/Moelf/Indicomb.jl/workflows/CI/badge.svg)](https://github.com/Moelf/Indicomb.jl/actions)
--- src/Indicomb.jl
+++ src/Indicomb.jl
@@ -2,4 +2,44 @@ module Indicomb
 
 # Write your package code here.
 
+"""
+indicomb.py - combs through indico and builds a listing of your meetings for display as a webpage or importing to iCal
+"""
+
+
+using JSON3, HTTP, Dates, SHA
+import DataStructures: OrderedDict
+import HTTP: escape
+
+# performe a one-time digest
+function sha1_hmac(key, data)
+    h = HMAC_CTX(SHA1_CTX(), Vector{UInt8}(key))
+    update!(h, Vector{UInt8}(data))
+    digest!(h)
+end
+
+"""
+    indico_request(path; params...)
+
+Build a path?params url object (without the base domain). The key feature is to construct `signature=` query
+based on the SHA1 HMAC algo. The construction of `signature` only cares about path, not base-site domain.
+"""
+function indico_request(path; params...)
+    d = OrderedDict{Symbol, Any}(params) # don't care about performance
+    if :secret_key ∈ keys(d)
+        # key is `secret_key` and data is entire url except signature=
+        _now = Dates.value(now(UTC)) - Dates.UNIXEPOCH
+        d[:timestamp] = _now ÷ 1000# ms -> s
+        sort!(d)
+        temp_url = "$path?$(escape(d))"
+        sig = bytes2hex(sha1_hmac(d[:secret_key], temp_url))
+        d[:signature] = sig
+    end
+    "$path?$(escape(d))"
+end
+
+function get_indico_page(baseurl, path; params...)
+    _u = "$baseurl$(indico_request(path; params...))"
+    HTTP.get(_u)
+end
 end
