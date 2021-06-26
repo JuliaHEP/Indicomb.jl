cef4f08bdfae67b36d3fd51078123e390313e8b2
Jerry Ling <proton@jling.dev>
2021-06-26 22:46:01 +0200

    update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,15 @@
 using Indicomb
 using Test, JSON3
 
-@testset "Indicomb.jl" begin
+@testset "Indico main function" begin
+    t = get_events_catnum_name("https://indico.cern.ch", 6725, r"."; from="2021-06-01", to="2021-06-15")
+    @test length(t) == 14
+    t = get_events_catnum_name("https://indico.cern.ch", 6725, "Edition"; from="2021-06-01", to="2021-06-15")
+    @test length(t) == 1
+    @test contains(t[1][:title], "Edition of the Large")
+end
+
+@testset "Indico utility functions" begin
     re = Indicomb.indico_request("cern.ch/"; p = 3)
     @test re == "cern.ch/?p=3"
 
@@ -11,9 +19,7 @@ using Test, JSON3
         from="2021-06-01",
         to="2021-06-15",
     )
-    j = JSON3.read(String(t.body))
-    @test ("Content-Type" => "application/json") ∈ t.headers
-    @test j[:count] == 14
-    @test j[:results][1][:_type] == "Conference"
-    @test contains(j[:results][1][:title], "Edition of the Large")
+    @test t[:count] == 14
+    @test t[:results][1][:_type] == "Conference"
+    @test contains(t[:results][1][:title], "Edition of the Large")
 end
