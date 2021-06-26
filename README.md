2a43d4df10f10b5aea0872e363761cc34fade08f
Jerry Ling <proton@jling.dev>
2021-06-26 22:39:20 +0200

    update README

--- README.md
+++ README.md
@@ -5,9 +5,52 @@
 [![Build Status](https://github.com/Moelf/Indicomb.jl/workflows/CI/badge.svg)](https://github.com/Moelf/Indicomb.jl/actions)
 
 # Quick Start
+
+At the most top level, this pkg export a single function `get_events_cat_name`, that can be used to get (detailed) Indico events within
+a category number, by events' `:title`:
 ```
-julia> t = Indicomb.get_indico_page("https://indico.cern.ch/", "/export/categ/1135.json";apikey="87495248-xxxxxxx", secret_key="20980c49-xxxxxx", from="2020-06-01", to="2030-06-01")
+julia> t = get_events_catnum_name("https://indico.cern.ch", 1X3X, "XXXXX"; from="2021-06-10", to="2021-06-30", apikey=".....", secretkey="....");
+
+julia> t[1]
+JSON3.Object{Vector{UInt8}, SubArray{UInt64, 1, Vector{UInt64}, Tuple{UnitRange{Int64}}, true}} with 29 entries:
+  :_type            => "Conference"
+  :id               => "10521XX"
+  :title            => "XXXXX group meeting"
+  :description      => ""
+  :startDate        => {…
+  :timezone         => "Europe/Zurich"
+  :endDate          => {…
+  :room             => ""
+  :location         => ""
+  :address          => ""
+  :type             => "meeting"
+  :references       => Union{}[]
+  :_fossil          => "conferenceMetadataWithSubContribs"
+  :categoryId       => 1X3X
+  :category         => "Harvard University"
+  :note             => {…
+  :roomFullname     => ""
+  :url              => "https://indico.cern.ch/event/10521XX/"
+  :creationDate     => {…
+  :creator          => {…
+  :hasAnyProtection => true
+  :roomMapURL       => nothing
+  :folders          => JSON3.Object[{…
+  :chairs           => Union{}[]
+  :material         => JSON3.Object[{…
+  :keywords         => Union{}[]
+  :visibility       => {…
+  :allowed          => {…
+  :contributions    => JSON3.Object[{…
+```
+
+There are lower level stuff (more like utility functons I guess) in case you want to hack around. Feel free to raise quality of life improvement feature request.
+```
+julia> Indicomb.get_indico_page("https://indico.cern.ch/", "/export/categ/1135.json";apikey=".....", secret_key="xxxxxx", from="2021-06-01", to="2030-06-01")
 HTTP.Messages.Response:
 """
 HTTP/1.1 200 OK
 ```
+
+# TODO
+[ ] Ship a HTML page generation script and a CSS
--- src/Indicomb.jl
+++ src/Indicomb.jl
@@ -1,23 +1,42 @@
 module Indicomb
 
+export get_events_catnum_name
+
 # Write your package code here.
 
 """
 indicomb.py - combs through indico and builds a listing of your meetings for display as a webpage or importing to iCal
 """
 
-
 using JSON3, HTTP, Dates, SHA
 import DataStructures: OrderedDict
 import HTTP: escapeuri
+import Base.Threads: @threads
 
-# performe a one-time update and then return digest
-function sha1_hmac(key, data)
-    h = HMAC_CTX(SHA1_CTX(), Vector{UInt8}(key))
-    update!(h, Vector{UInt8}(data))
-    digest!(h)
+_format_dt(dt) = Dates.format(dt, dateformat"y-m-d")
+
+function get_events_catnum_name(
+    baseurl,
+    cat_num,
+    evt_name;
+    params...,
+)
+
+    # get category JSON which has events in simplr form
+    cat = get_indico_page(baseurl, "/export/categ/$cat_num.json"; params...)
+    simple_evts = filter_events(evt_name, cat)
+    res = Vector{JSON3.Object}(undef, length(simple_evts))
+
+    # add event specific headers to into original `params` to get full info
+    detail_params = (pretty="yes", detail="subcontributions", params...)
+    @threads for idx in eachindex(simple_evts)
+        id = simple_evts[idx][:id]
+        evt = get_indico_page(baseurl, "/export/event/$id.json"; detail_params...)
+        # stupid Indico always return bloated/nested JSON, unpact here!
+        res[idx] = only(evt[:results]) #`only` because we're quering a single event
+    end
+    return res
 end
-
 """
     indico_request(path; params...)
 
@@ -25,23 +44,37 @@ Build a path?params url object (without the base domain). The key feature is to 
 based on the SHA1 HMAC algo. The construction of `signature` only cares about path, not base-site domain.
 """
 function indico_request(path; params...)
-    d = OrderedDict{Symbol, Any}(params) # don't care about performance
-    if :secret_key ∈ keys(d)
+    d = OrderedDict{Symbol,Any}(params) # don't care about performance
+    if :secretkey ∈ keys(d)
         # key is `secret_key` and data is entire url except signature=
         _now = Dates.value(now(UTC)) - Dates.UNIXEPOCH
         d[:timestamp] = _now ÷ 1000# ms -> s
         sort!(d)
         temp_url = "$path?$(escapeuri(d))"
-        sig = bytes2hex(sha1_hmac(d[:secret_key], temp_url))
+        sig = bytes2hex(sha1_hmac(d[:secretkey], temp_url))
         d[:signature] = sig
     end
-    "$path?$(escapeuri(d))"
+    return "$path?$(escapeuri(d))"
 end
 
 function get_indico_page(baseurl, path; params...)
     _u = "$baseurl$(indico_request(path; params...))"
-    HTTP.get(_u)
+    return JSON3.read(HTTP.get(_u).body)
 end
 
+function filter_events(f::Function, category)
+    return filter(f, category[:results])
+end
+
+function filter_events(title_name::Union{AbstractString,Regex}, category)
+    return filter_events(x -> contains(x[:title], title_name), category)
+end
+
+# performe a one-time update and then return digest
+function sha1_hmac(key, data)
+    h = HMAC_CTX(SHA1_CTX(), Vector{UInt8}(key))
+    update!(h, Vector{UInt8}(data))
+    return digest!(h)
+end
 
 end
